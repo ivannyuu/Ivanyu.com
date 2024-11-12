1b2e7618b2878df7a830ee499d8b312969bb1376
ivannyuu <ivannyuu>
2024-11-12 14:30:41 -0800

    added comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */ //tells IDE that the configuration object adheres to the expected structure of a Tailwind CSS configuration file
 export default {
+  // 'content' tells Tailwind where to look for classes to purge unused CSS.
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  // 'theme' is where you can extend or customize Tailwind's default settings.
   theme: {
     extend: {},
   },
-  plugins: [],
+  // 'plugins' lets you add extra features to Tailwind.
+  plugins: [], // Empty for now, but you can add plugins later.
 }
 
