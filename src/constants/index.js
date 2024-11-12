50820a524d46fcf304ed3f8694cc4d6737798af0
ivannyuu <ivannyuu>
2024-11-12 11:28:15 -0800

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,102 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const ABOUT_TEXT = `I’m a previous kinesiology major turned computer science student at SFU where I discovered a passion for technology and innovation. The blend of problem-solving and creativity excites me, as it allows me to transform ideas into functional and impactful code.
+
+I thrive in collaborative, cross-functional teams, working together to build efficient and user-friendly applications that make a real difference. I’m always eager to connect, share stories, and learn from others – so don’t hesitate to reach out!
+`;
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "Lead Kinesiologist",
+    company: "CBI",
+    location: "Vancouver, BC",
+    description: `Collaborated with multidisciplinary healthcare teams to create personalized treatment plans tailored to each client’s unique needs. Supported individuals with diverse abilities, including Autism, developmental disorders, and brain injuries.`,
+  },
+  {
+    year: "2022 - 2023",
+    role: "Laboratory Technician",
+    company: "Nautilus Environmental",
+    location: "Vancouver, BC",
+    description: `Problem-solved and troubleshot laboratory experiments, utilizing critical thinking and analytical skills to resolve issues and complete tasks on time.`,
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "PAIN2GO",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "Javascript"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["React", "Tailwind CSS", "Framer Motion"],
+  },
+];
+
+export const CONTACT = {
+  address: "767 Fifth Avenue, New York, NY 10153 ",
+  phoneNo: "+12 4555 666 00 ",
+  email: "yuivan2000@gmail.com",
+  linkedin: "https://www.linkedin.com/in/ivannyuu",
+  github: "https://github.com/ivannyuu",
+};
+
+export const EDUCATION = [
+  {
+    degree: "B.Sc. in Computer Science",
+    institution: "Simon Fraser University",
+    location: "Vancouver, BC",
+    expectedGraduation: "Dec 2025",
+    coursework: [
+      "Computing in Python",
+      "Data Structures & Algorithms",
+      "Computer Architecture",
+      "Combinatorics",
+      "Object-Oriented Programming",
+      "Computer Vision & Graphics",
+    ],
+  },
+  {
+    degree: "B.Sc. in Kinesiology",
+    institution: "Simon Fraser University",
+    location: "Vancouver, BC",
+    graduation: "Jun 2023",
+    concentration: "Active Health and Rehabilitation",
+  },
+];
+
+export const SKILLS = [
+  "Python",
+  "JavaScript",
+  "HTML/CSS",
+  "React.js",
+  "MATLAB",
+  "C++",
+  "C",
+  "Git",
+  "Github",
+];
+
+export const ACTIVITIES = [
+  {
+    role: "Peer Tutor",
+    organization: "Simon Fraser University",
+    duration: "Sep 2024 - Present",
+    courses: ["CMPT120", "CMPT125", "MACM 101"],
+  },
+  {
+    role: "Athletic Trainer",
+    organization: "Simon Fraser University",
+    duration: "Jan 2022 - Apr 2022",
+  },
+];
+
